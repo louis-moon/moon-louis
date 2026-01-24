550113f679119f2b27251f76c9674f5945c5592b
louis-moon <louismoon23@gmail.com>
2026-01-24 03:22:19 -0500

    temp

--- app/professional/page.tsx
+++ app/professional/page.tsx
@@ -93,8 +93,8 @@ const fullTime: Company[] = [
         type: "Full-time",
         period: "Jan 2023 – Mar 2024 • 1 yr 3 mos",
         bullets: [
-          "Established a semantic comparable business search query system to value businesses through comparable company analyses; formulated one version for the sell-side and one for the buy-side, bringing the time for process down from hours to seconds",
-          "Led product development initiatives for the financial valuation product, focusing on core functionalities and user experience; created and maintained comprehensive product roadmap, prioritizing key features for MVP development and future iterations",
+          "Utilized 3D CNN encoder and RNN decoder technology to build a video-to-text model with logistics clients for integration; focused on industry-specific use cases including package delivery verification, employee monitoring, and driver safety checks",
+          "Co-founded organization alongside Yale, Berkeley, and UChicago alums with a focus on product and software development; leveraged children's books to build a text-to-video model as a complementary solution using natural language processing tools",
         ],
       },
     ],
